330a96018c3bd322ec314cb854d0699886bf3b6b
Raul Almanza <ra.almanzaserrano@gmail.com>
2020-06-15 22:35:44 -0500

    Work with the game of life without if conditions nor cycles

---
+++ game_of_life.rb
@@ -0,0 +1,68 @@
+# Indeed when you use "times", "each", "map", etc., you are using cycles
+# i don't know any other way to do it :(
+
+class GameOfLife
+  attr_accessor :world, :neighborhood
+
+  def initialize(size)
+    @size = size
+    @world = Array.new(size){Array.new(size)} 
+    @neighborhood  = Array.new(size){Array.new(size)}
+  end
+
+  def populate
+    @size.times do |row|
+      @size.times do |col|
+        world[row][col] = ['_', '1'].sample
+        neighborhood[row][col] = 0
+      end
+    end
+  end
+
+  def print_world
+    world.each { |elem| puts "#{elem}" }
+  end
+
+  def print_neighborhood
+    neighborhood.each { |elem| puts "#{elem}" }
+  end
+
+  def next_step
+    values = (1..@size-2).to_a
+    values.each do |row|
+      values.each do |col|
+        counter=0
+        counter += 1 unless world[row-1][col] == '_'
+        counter += 1 unless world[row-1][col-1] == '_'
+        counter += 1 unless world[row-1][col+1] == '_'
+        counter += 1 unless world[row][col-1] == '_'
+        counter += 1 unless world[row][col+1] == '_'
+        counter += 1 unless world[row+1][col-1] == '_'
+        counter += 1 unless world[row+1][col+1] == '_'
+        counter += 1 unless world[row+1][col] == '_'
+
+        neighborhood[row][col] = counter
+      end   
+    end
+  end 
+
+  def find_life
+    @size.times do |row|
+      @size.times do |col|
+        world[row][col] = (neighborhood[row][col] == 2 || neighborhood[row][col] == 3) ? '1' : '_' 
+      end
+    end  
+  end 
+end 
+
+game = GameOfLife.new(10)
+game.populate
+game.print_world
+game.next_step
+puts "_____________________"
+puts "Neighborhood Matrix"
+game.print_neighborhood
+puts "---------------------"
+puts "Next generation"
+game.find_life
+game.print_world
